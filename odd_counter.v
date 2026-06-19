9053a8166793c37678653d94b65f318de41dc6c9
MayankPaliwal tech <paliwalmayank01080312@gmail.com>
2026-06-19 22:30:55 +0530

    added odd_counter code

---
+++ odd_counter.v
@@ -0,0 +1,18 @@
+// Odd counter
+
+module day5 (
+  input     wire        clk,
+  input     wire        reset,
+
+  output    logic[7:0]  cnt_o
+);
+
+  always@(posedge clk or posedge reset) begin
+    if(reset) begin
+      cnt_o <= 8'h1;
+    end
+    else
+      cnt_o <= cnt_o +2;
+  end
+
+endmodule
